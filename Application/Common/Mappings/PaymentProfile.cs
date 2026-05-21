edd9a58703b3551c02d35632da213806aa0f9f8f
pavi <test@123gmail.com>
2026-05-21 18:28:04 +0530

    Added PaymentMapper for PaymentDTO

---
+++ Application/Common/Mappings/PaymentProfile.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Mappings
+{
+    public class PaymentProfile : Profile
+    {
+        public PaymentProfile() 
+        {
+            CreateMap<Payment, PaymentDto>();
+
+        }
+    }
+}
